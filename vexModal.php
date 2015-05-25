316c8226b2b7881592c8e3860a1ba4c177b8e5d0
Winston Riley <masonwr@mac.com>
2015-05-24 18:32:59 -0600

    got the basics working

--- vexModal.php
+++ vexModal.php
@@ -8,23 +8,17 @@ Author:      winston
  */
 
 
-// $pluginDIR = plugin_dir_path( __FILE__ );
 $pluginDIR = "/wp-content/plugins/vexModal/";
-
-
-function registerVexStyle()
-{
-  global $pluginDIR;
-  wp_enqueue_script('vex', $pluginDIR . "js/vex.combined.min.js", array('jquery'));
-  wp_enqueue_style("vex-theme-os", $pluginDIR . "css/vex-theme-os.css");
-  wp_enqueue_style("vex-base", $pluginDIR . "css/vex.css");
-}
-add_action('wp_enqueue_scripts', 'registerVexStyle');
 
 
 function showModal()
 {
   global $pluginDIR;
+
+  wp_enqueue_style("vex-theme-os", $pluginDIR . "css/vex-theme-os.css");
+  wp_enqueue_style("vex-base", $pluginDIR . "css/vex.css");
+
+  wp_enqueue_script('vex', $pluginDIR . "js/vex.combined.min.js", array('jquery'));
   wp_enqueue_script('showModal', $pluginDIR . "js/showModal.js", array('vex'));  
 }
 
