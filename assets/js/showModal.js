638c9b280f1e96184465bd5728f947ceabd30192
Winston Riley <masonwr@mac.com>
2015-05-30 19:54:37 -0600

    the user can now select the background color and color opacity

--- assets/js/showModal.js
+++ assets/js/showModal.js
@@ -4,20 +4,49 @@
 'use strict';
 jQuery.noConflict();
 
-/**
- * Pretty basic at this point, just a developer hook into the Vex lib
- */
 vex.defaultOptions.className = wp_vars.vexStyle;
 vex.defaultOptions.showCloseButton = true;
+vex.defaultOpacity = .6;
+
+/**
+ * Parse a hex string in the format of "#xxxxxx"
+ * into a color obj
+ *
+ * "#xxxxxx" -> color {r, g, b}
+ */
+var vexHexToRGBColorParser = function(hex){
+  var color = {};
+  color.r = parseInt("0x" + hex.slice(1,3));
+  color.g = parseInt("0x" + hex.slice(3,5));
+  color.b = parseInt("0x" + hex.slice(5,7));
+  return color;
+}
+
+/**
+ * clamps the specified value beween 0 and 1.
+ */
+var clamp = function(val) {
+  if (isNaN(val)) return vex.defaultOpacity;
+  return Math.max(0, Math.min(1, val) );
+}
 
 var vexShowSlide = function(call_back) {
 	vex.dialog.buttons.YES.text = wp_vars.vexBtnYes;
 	vex.dialog.buttons.NO.text = wp_vars.vexBtnNo;
 
+  var color = vexHexToRGBColorParser(wp_vars.vexOverlayStyle);
+  var opacity = clamp(parseFloat(wp_vars.opacity));
+  
+  // building up the css => "rgba(r,g,b,a)"
+  var colorStr = "rgba("+color.r;
+  colorStr += "," + color.g;
+  colorStr += "," + color.b;
+  colorStr += "," + opacity + ")"
+
 	vex.dialog.confirm({
 		message: wp_vars.message,
 		overlayCSS: {
-			'background': wp_vars.vexOverlayStyle
+			'background': colorStr,
 		},
 		callback: call_back
 	});
@@ -26,3 +55,4 @@ var vexShowSlide = function(call_back) {
 jQuery(document).ready(function($) {
 	vexShowSlide();
 });
+
